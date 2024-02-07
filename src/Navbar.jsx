3b743d6537ea47da687b37b3c4d9f6c713ad6c77
raman5911 <ramanpreets529@gmail.com>
2024-02-07 12:13:59 +0530

    built navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='nav'>
+        <img className='netflix_logo' src='./netflix-logo.png' />
+        <img  className='guest_user' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Guest User" />
+    </div>
   )
 }
 
